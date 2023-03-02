5af567df21e095eabc2079cb91c172a28527825a
Jimmy-sheep <jimmysheep0420@gmail.com>
2023-03-02 18:29:01 +0800

    Fix the range typo

--- HW1/main8.c
+++ HW1/main8.c
@@ -8,7 +8,7 @@ void reverse(char* ch, int size) {
     for (int i = size - 1; i >= 0; i--) {
         if ((ch[i] >= 97 && ch[i] <= 122) && (ch[j] >= 41 && ch[j] <= 90))
             printf("%c", ch[i] - 32);
-        else if ((ch[i] >= 41 && ch[i] <= 90) && (ch[j] >= 97 && ch[j] <= 122))
+        else if ((ch[i] >= 65 && ch[i] <= 90) && (ch[j] >= 97 && ch[j] <= 122))
             printf("%c", ch[i] + 32);
         else
             printf("%c", ch[i]);
