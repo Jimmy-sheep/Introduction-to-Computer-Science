5b994b73214585a2943fe00670126c64ad08f89f
Jimmy-sheep <jimmysheep0420@gmail.com>
2023-03-09 14:20:33 +0800

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -7,14 +7,14 @@ int main() {
 	char b[] = "ABC";
 	float c[] = { 1.1, 1.2, 1.3 };
 	FILE* pFile;
-	int arr_read[3];
+	int arr_read[3] = { 0 };
 
 	pFile = fopen("a.bin", "wb+");
-	fwrite(a, sizeof(int), 1, pFile);
+	fwrite(a, sizeof(a), 1, pFile);
 	fseek(pFile, 0, SEEK_SET);
 	fread(arr_read, sizeof(arr_read), 1, pFile);
-
 	fclose("a.bin");
+
 	for (int i = 0; i < 3; i++)
 		printf("%d", arr_read[i]);
 
