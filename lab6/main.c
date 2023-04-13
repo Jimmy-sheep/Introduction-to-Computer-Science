83885ddafca3c9ea7b9aa354c5edc4f59188a54b
Jimmy-sheep <jimmysheep0420@gmail.com>
2023-04-13 15:15:58 +0800

    Fix typo

--- lab6/main.c
+++ lab6/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int multiplacation(int i, int j) {
+int multiplication(int i, int j) {
 	if (i == 9 && j == 9)
 		printf("%d*%d=%d\n", i, j, i * j);
 	else if (j == 9) {
 		printf("%d*%d=%d\n", i, j, i * j);
-		multiplacation(i + 1, 1);
+		multiplication(i + 1, 1);
 	}
 	else {
 		printf("%d*%d=%d ", i, j, i * j);
-		multiplacation(i, j + 1);
+		multiplication(i, j + 1);
 	}
 }
 
 int main() {
-	multiplacation(1, 1);
+	multiplication(1, 1);
 
 	return 0;
 }
